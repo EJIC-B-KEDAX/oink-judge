b9118a7f4c7870490eca0288de4ba3529c421a7d
kuznezov <iozh.v.kedah228@gmail.com>
2025-12-24 23:51:31 +0300

    Start work on web, change problems structure, now every problem is class instance with own router.

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from app.routes.web import auth, main
-from app.routes.web.problems import default_problem
+from app.routes.web.problems import open_problem
+from app.config.config import Config
+import os
 
 
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(main.router)
-app.include_router(default_problem.router)
+
+for problem_id in os.listdir(Config["directories"]["problems"]):
+    problem = open_problem.OpenProblem(problem_id) # TODO handle different problem types (make factory function)
+    app.include_router(problem.router)
